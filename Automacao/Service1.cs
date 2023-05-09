3b362f84a25fded1b8079af3c7a716609a8a8fe5
LuanCunha05 <69827695+LuanCunha05@users.noreply.github.com>
2023-05-09 11:25:13 -0400

    Add files via upload

---
+++ Automacao/Service1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using Timer = System.Timers.Timer;
+
+namespace Automacao
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer timer;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+                // Cria um timer com intervalo de X horas/minutos e por vai
+                timer = new Timer(4 * 60 * 1000);
+                timer.Elapsed += OnTimedEvent;
+                timer.AutoReset = true;
+                timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+                timer.Dispose();
+        }
+
+        private void OnTimedEvent(object sender, ElapsedEventArgs e)
+        {
+                // Coloque aqui o comando que você quer executar a cada X horas
+                // Por exemplo:
+                string origem = @"C:\Users\03461471232\Documents\Arquivosparaler";
+                string destino = @"D:\Nova pasta";
+
+                Directory.CreateDirectory(destino);
+
+                foreach (string dirPath in Directory.GetDirectories(origem, "*", SearchOption.AllDirectories))
+                    Directory.CreateDirectory(dirPath.Replace(origem, destino));
+
+                foreach (string filePath in Directory.GetFiles(origem, ".", SearchOption.AllDirectories))
+                    File.Copy(filePath, filePath.Replace(origem, destino), true);     
+        }
+    }
+}
+
